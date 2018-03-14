698bfc12fa5ef305dad3a0e64613dba338c24cec
Rob Tsuk <robtsuk@google.com>
2018-03-14 13:48:47 -0700

    WIP
    Change-Id: Id1e5d529e762c40a1d5ea76b6d7a6014549ff961

---
+++ public/rust/fidl_crates/garnet_public_lib_ui_mozart_fidl_presentation_info/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#![deny(warnings)]
+#![deny(missing_debug_implementations)]
+#![allow(unused_imports)]
+#![allow(unused_extern_crates)]
+
+include!(concat!(env!("FUCHSIA_GEN_ROOT"), "/garnet/public/lib/ui/mozart/fidl/garnet_public_lib_ui_mozart_fidl_presentation_info.rs"));
