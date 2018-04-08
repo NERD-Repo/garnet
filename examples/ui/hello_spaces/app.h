010c26adbe05b9f4fa06dc310c2a29368007e99d
David Worsham <dworsham@google.com>
2018-04-07 20:52:16 -0700

    [scenic] Initial running client/server apps
    Apps run in the same process on different threads.  For some reason the
    service connection isn't working yet :(
    
    SCN-601 #comment
    
    Change-Id: Id3abbabf44d106a82c06aaaaddcb26017f39a939

--- examples/ui/hello_spaces/app.h
+++ examples/ui/hello_spaces/app.h
@@ -5,23 +5,24 @@
 #ifndef GARNET_EXAMPLES_UI_HELLO_SPACES_APP_H_
 #define GARNET_EXAMPLES_UI_HELLO_SPACES_APP_H_
 
+#include <fuchsia/cpp/ui.h>
+
 #include "lib/app/cpp/application_context.h"
-
 #include "lib/fsl/tasks/message_loop.h"
-
 #include "lib/ui/scenic/client/resources.h"
 #include "lib/ui/scenic/client/session.h"
 
 namespace hello_spaces {
 
+enum class AppType : unsigned char { Controller, Guest };
+
 class App {
  public:
-  App();
+  explicit App(AppType type);
+
+  const char* AppIdentifier() const;
 
  private:
-  // Called asynchronously by constructor.
-  void Init(gfx::DisplayInfo display_info);
-
   // Called asynchronously when the session dies.
   void ReleaseSessionResources();
 
@@ -30,8 +31,10 @@ class App {
   // presentation is asynchronously received.
   void Update(uint64_t next_presentation_time);
 
-  void CreateExampleScene(float display_width, float display_height);
+  // Creates all of the scene resources and sets up the scene graph.
+  void CreateScene(float display_width, float display_height);
 
+  AppType type_;
   std::unique_ptr<component::ApplicationContext> application_context_;
   fsl::MessageLoop* loop_;
 
@@ -39,6 +42,7 @@ class App {
   std::unique_ptr<scenic_lib::Session> session_;
   std::unique_ptr<scenic_lib::DisplayCompositor> compositor_;
   std::unique_ptr<scenic_lib::Camera> camera_;
+  std::unique_ptr<ui::SpaceProvider> space_provider_;
 
   FXL_DISALLOW_COPY_AND_ASSIGN(App);
 };
--- examples/ui/hello_spaces/main.cc
+++ examples/ui/hello_spaces/main.cc
@@ -2,25 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuchsia/cpp/ui.h>
+#include <thread>
 
 #include "garnet/examples/ui/hello_spaces/app.h"
 #include "lib/fsl/tasks/message_loop.h"
 #include "lib/fxl/command_line.h"
 #include "lib/fxl/log_settings_command_line.h"
 
+void run_app(hello_spaces::AppType type) {
+  fsl::MessageLoop loop;
+  hello_spaces::App app(type);
+  loop.task_runner()->PostDelayedTask(
+      [&app, &loop] {
+        FXL_LOG(INFO) << app.AppIdentifier() << " HARD Quitting.";
+        loop.QuitNow();
+      },
+      fxl::TimeDelta::FromSeconds(20));
+  loop.Run();
+}
+
 int main(int argc, const char** argv) {
   auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
   if (!fxl::SetLogSettingsFromCommandLine(command_line)) return 1;
 
-  fsl::MessageLoop loop;
-  hello_spaces::App app;
-  loop.task_runner()->PostDelayedTask(
-      [&loop] {
-        FXL_LOG(INFO) << "Quitting.";
-        loop.QuitNow();
-      },
-      fxl::TimeDelta::FromSeconds(50));
-  loop.Run();
+  // Run the controller and any client(s) on their own threads, so that each one
+  // can have its own MessageLoop.
+  std::thread controller_thread(run_app, hello_spaces::AppType::Controller);
+  std::thread guest_thread(run_app, hello_spaces::AppType::Guest);
+  controller_thread.join();
+  guest_thread.join();
+
   return 0;
 }
