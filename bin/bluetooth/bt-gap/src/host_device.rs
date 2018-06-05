vsrinivas/fuchsia

[bt][host][fidl] merge bluetooth host and adapter interfaces
NET-666 #done

Change-Id: I90f771a9cef52001ec70b6e69dda923909636c0d

bin/bluetooth/bt-gap/src/host_device.rs
@@ -5,26 +5,24 @@
 use fidl;
 use fidl_bluetooth::Status;
 use fidl_bluetooth_control::AdapterInfo;
-use fidl_bluetooth_host::{AdapterEvent, AdapterEventStream, AdapterProxy, HostProxy};
+use fidl_bluetooth_host::{HostEvent, HostEventStream, HostProxy};
 use futures::{FutureExt, StreamExt};
 use futures::Future;
 use futures::future::ok as fok;
 use host_dispatcher::HostDispatcher;
 use parking_lot::RwLock;
 use std::sync::Arc;
-use util::clone_adapter_state;
+use util::clone_host_state;
 
 pub struct HostDevice {
     host: HostProxy,
-    adapter: AdapterProxy,
     info: AdapterInfo,
 }
 
 impl HostDevice {
-    pub fn new(host: HostProxy, adapter: AdapterProxy, info: AdapterInfo) -> Self {
+    pub fn new(host: HostProxy, info: AdapterInfo) -> Self {
         HostDevice {
             host,
-            adapter,
             info,
         }
     }
@@ -38,11 +36,11 @@ impl HostDevice {
     }
 
     pub fn set_name(&self, mut name: String) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.set_local_name(&mut name)
+        self.host.set_local_name(&mut name)
     }
 
     pub fn start_discovery(&mut self) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.start_discovery()
+        self.host.start_discovery()
     }
 
     pub fn close(&self) -> Result<(), fidl::Error> {
@@ -50,28 +48,28 @@ impl HostDevice {
     }
 
     pub fn stop_discovery(&self) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.stop_discovery()
+        self.host.stop_discovery()
     }
 
     pub fn set_discoverable(
         &mut self, discoverable: bool
     ) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.set_discoverable(discoverable)
+        self.host.set_discoverable(discoverable)
     }
 }
 
-pub fn run_host_device(
-    hd: Arc<RwLock<HostDispatcher>>, adapter: Arc<RwLock<HostDevice>>
-) -> impl Future<Item = AdapterEventStream, Error = fidl::Error> {
-    make_clones!(adapter => stream_adapter, adapter);
-    let stream = stream_adapter.read().adapter.take_event_stream();
+pub fn run(
+    hd: Arc<RwLock<HostDispatcher>>, host: Arc<RwLock<HostDevice>>
+) -> impl Future<Item = HostEventStream, Error = fidl::Error> {
+    make_clones!(host => host_stream, host);
+    let stream = host_stream.read().host.take_event_stream();
     stream
         .for_each(move |evt| {
             match evt {
-                AdapterEvent::OnAdapterStateChanged { ref state } => {
-                    adapter.write().info.state = Some(Box::new(clone_adapter_state(&state)));
+                HostEvent::OnHostStateChanged { ref state } => {
+                    host.write().info.state = Some(Box::new(clone_host_state(&state)));
                 }
-                AdapterEvent::OnDeviceDiscovered { mut device } => {
+                HostEvent::OnDeviceDiscovered { mut device } => {
                     if let Some(ref events) = hd.write().events {
                         let _res = events
                             .send_on_device_updated(&mut device)

drivers/bluetooth/host/fidl/host_server.cc
@@ -4,20 +4,32 @@
 
 #include "host_server.h"
 
-#include "garnet/drivers/bluetooth/host/gatt_host.h"
+#include "garnet/drivers/bluetooth/lib/gap/adapter.h"
 
-#include "adapter_server.h"
+#include "garnet/drivers/bluetooth/host/gatt_host.h"
+#include "garnet/drivers/bluetooth/lib/gap/bredr_connection_manager.h"
+#include "garnet/drivers/bluetooth/lib/gap/bredr_discovery_manager.h"
+#include "garnet/drivers/bluetooth/lib/gap/gap.h"
+#include "garnet/drivers/bluetooth/lib/gap/low_energy_discovery_manager.h"
+#include "lib/fxl/logging.h"
+
 #include "helpers.h"
 #include "low_energy_central_server.h"
 #include "low_energy_peripheral_server.h"
 
 namespace bthost {
+using bluetooth::Bool;
+using bluetooth::ErrorCode;
+using bluetooth::Status;
+
+using bluetooth_control::AdapterState;
 
 HostServer::HostServer(zx::channel channel,
                        fxl::WeakPtr<::btlib::gap::Adapter> adapter,
                        fbl::RefPtr<GattHost> gatt_host)
     : AdapterServerBase(adapter, this, std::move(channel)),
-      gatt_host_(gatt_host) {
+      gatt_host_(gatt_host),
+      weak_ptr_factory_(this) {
   FXL_DCHECK(gatt_host_);
 }
 
@@ -25,9 +37,179 @@ void HostServer::GetInfo(GetInfoCallback callback) {
   callback(fidl_helpers::NewAdapterInfo(*adapter()));
 }
 
-void HostServer::RequestAdapter(
-    fidl::InterfaceRequest<bluetooth_host::Adapter> request) {
-  BindServer<AdapterServer>(std::move(request));
+void HostServer::SetLocalName(::fidl::StringPtr local_name,
+                              SetLocalNameCallback callback) {
+  adapter()->SetLocalName(local_name, [self = weak_ptr_factory_.GetWeakPtr(),
+                                       callback](auto status) {
+    callback(fidl_helpers::StatusToFidl(status, "Can't Set Local Name"));
+  });
+}
+
+void HostServer::StartDiscovery(StartDiscoveryCallback callback) {
+  FXL_VLOG(1) << "Adapter StartDiscovery()";
+  FXL_DCHECK(adapter());
+
+  if (le_discovery_session_ || requesting_discovery_) {
+    FXL_VLOG(1) << "Discovery already in progress";
+    callback(fidl_helpers::NewFidlError(ErrorCode::IN_PROGRESS,
+                                        "Discovery already in progress"));
+    return;
+  }
+
+  requesting_discovery_ = true;
+  auto bredr_manager = adapter()->bredr_discovery_manager();
+  // TODO(jamuraa): start these in parallel instead of sequence
+  bredr_manager->RequestDiscovery([self = weak_ptr_factory_.GetWeakPtr(),
+                                   bredr_manager, callback](
+                                      btlib::hci::Status status, auto session) {
+    if (!self) {
+      callback(
+          fidl_helpers::NewFidlError(ErrorCode::FAILED, "Adapter Shutdown"));
+      return;
+    }
+
+    if (!status || !session) {
+      FXL_VLOG(1) << "Failed to start BR/EDR discovery session";
+      callback(fidl_helpers::StatusToFidl(
+          status, "Failed to start BR/EDR discovery session"));
+      self->requesting_discovery_ = false;
+      return;
+    }
+
+    session->set_result_callback([self](const auto& device) {
+      if (self) {
+        self->OnDiscoveryResult(device);
+      }
+    });
+
+    self->bredr_discovery_session_ = std::move(session);
+
+    auto le_manager = self->adapter()->le_discovery_manager();
+    le_manager->StartDiscovery([self, callback](auto session) {
+      // End the new session if this AdapterServer got destroyed in the mean
+      // time (e.g. because the client disconnected).
+      if (!self) {
+        callback(
+            fidl_helpers::NewFidlError(ErrorCode::FAILED, "Adapter Shutdown"));
+        return;
+      }
+
+      if (!session) {
+        FXL_VLOG(1) << "Failed to start LE discovery session";
+        callback(fidl_helpers::NewFidlError(
+            ErrorCode::FAILED, "Failed to start LE discovery session"));
+        self->bredr_discovery_session_ = nullptr;
+        self->requesting_discovery_ = false;
+        return;
+      }
+
+      // Set up a general-discovery filter for connectable devices.
+      session->filter()->set_connectable(true);
+      session->filter()->SetGeneralDiscoveryFlags();
+      session->SetResultCallback([self](const auto& device) {
+        if (self) {
+          self->OnDiscoveryResult(device);
+        }
+      });
+
+      self->le_discovery_session_ = std::move(session);
+      self->requesting_discovery_ = false;
+
+      // Send the adapter state update.
+      AdapterState state;
+      state.discovering = bluetooth::Bool::New();
+      state.discovering->value = true;
+      self->binding()->events().OnHostStateChanged(std::move(state));
+
+      callback(Status());
+    });
+  });
+}
+
+void HostServer::StopDiscovery(StopDiscoveryCallback callback) {
+  FXL_VLOG(1) << "Adapter StopDiscovery()";
+  if (!le_discovery_session_) {
+    FXL_VLOG(1) << "No active discovery session";
+    callback(fidl_helpers::NewFidlError(ErrorCode::BAD_STATE,
+                                        "No discovery session in progress"));
+    return;
+  }
+
+  bredr_discovery_session_ = nullptr;
+  le_discovery_session_ = nullptr;
+
+  AdapterState state;
+  state.discovering = bluetooth::Bool::New();
+  state.discovering->value = false;
+  this->binding()->events().OnHostStateChanged(std::move(state));
+
+  callback(Status());
+}
+
+void HostServer::SetConnectable(bool connectable,
+                                SetConnectableCallback callback) {
+  FXL_VLOG(1) << "Adapter SetConnectable(" << connectable << ")";
+
+  adapter()->bredr_connection_manager()->SetConnectable(
+      connectable, [callback](const auto& status) {
+        callback(fidl_helpers::StatusToFidl(status));
+      });
+}
+
+void HostServer::SetDiscoverable(bool discoverable,
+                                 SetDiscoverableCallback callback) {
+  FXL_VLOG(1) << "Adapter SetDiscoverable(" << discoverable << ")";
+  // TODO(NET-830): advertise LE here
+  if (!discoverable) {
+    bredr_discoverable_session_ = nullptr;
+
+    AdapterState state;
+    state.discoverable = bluetooth::Bool::New();
+    state.discoverable->value = false;
+    this->binding()->events().OnHostStateChanged(std::move(state));
+
+    callback(Status());
+  }
+  if (discoverable && requesting_discoverable_) {
+    FXL_VLOG(1) << "Discoverable already being set";
+    callback(fidl_helpers::NewFidlError(ErrorCode::IN_PROGRESS,
+                                        "Discovery already in progress"));
+    return;
+  }
+  requesting_discoverable_ = true;
+  auto bredr_manager = adapter()->bredr_discovery_manager();
+  bredr_manager->RequestDiscoverable(
+      [self = weak_ptr_factory_.GetWeakPtr(), callback](
+          btlib::hci::Status status, auto session) {
+        if (!self) {
+          callback(fidl_helpers::NewFidlError(ErrorCode::FAILED,
+                                              "Adapter Shutdown"));
+          return;
+        }
+        if (!status || !session) {
+          FXL_VLOG(1) << "Failed to set discoverable!";
+          callback(
+              fidl_helpers::StatusToFidl(status, "Failed to set discoverable"));
+          self->requesting_discoverable_ = false;
+        }
+        self->bredr_discoverable_session_ = std::move(session);
+        AdapterState state;
+        state.discoverable = bluetooth::Bool::New();
+        state.discoverable->value = true;
+        self->binding()->events().OnHostStateChanged(std::move(state));
+        callback(Status());
+      });
+}
+
+void HostServer::OnDiscoveryResult(
+    const ::btlib::gap::RemoteDevice& remote_device) {
+  auto fidl_device = fidl_helpers::NewRemoteDevice(remote_device);
+  if (!fidl_device) {
+    FXL_VLOG(1) << "Ignoring malformed discovery result";
+    return;
+  }
+
+  this->binding()->events().OnDeviceDiscovered(std::move(*fidl_device));
 }
 
 void HostServer::RequestLowEnergyCentral(

NERD-Repo/garnet

bin/bluetooth/bt-gap/src/host_device.rs
@@ -5,26 +5,24 @@
 use fidl;
 use fidl_bluetooth::Status;
 use fidl_bluetooth_control::AdapterInfo;
-use fidl_bluetooth_host::{AdapterEvent, AdapterEventStream, AdapterProxy, HostProxy};
+use fidl_bluetooth_host::{HostEvent, HostEventStream, HostProxy};
 use futures::{FutureExt, StreamExt};
 use futures::Future;
 use futures::future::ok as fok;
 use host_dispatcher::HostDispatcher;
 use parking_lot::RwLock;
 use std::sync::Arc;
-use util::clone_adapter_state;
+use util::clone_host_state;
 
 pub struct HostDevice {
     host: HostProxy,
-    adapter: AdapterProxy,
     info: AdapterInfo,
 }
 
 impl HostDevice {
-    pub fn new(host: HostProxy, adapter: AdapterProxy, info: AdapterInfo) -> Self {
+    pub fn new(host: HostProxy, info: AdapterInfo) -> Self {
         HostDevice {
             host,
-            adapter,
             info,
         }
     }
@@ -38,11 +36,11 @@ impl HostDevice {
     }
 
     pub fn set_name(&self, mut name: String) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.set_local_name(&mut name)
+        self.host.set_local_name(&mut name)
     }
 
     pub fn start_discovery(&mut self) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.start_discovery()
+        self.host.start_discovery()
     }
 
     pub fn close(&self) -> Result<(), fidl::Error> {
@@ -50,28 +48,28 @@ impl HostDevice {
     }
 
     pub fn stop_discovery(&self) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.stop_discovery()
+        self.host.stop_discovery()
     }
 
     pub fn set_discoverable(
         &mut self, discoverable: bool
     ) -> impl Future<Item = Status, Error = fidl::Error> {
-        self.adapter.set_discoverable(discoverable)
+        self.host.set_discoverable(discoverable)
     }
 }
 
-pub fn run_host_device(
-    hd: Arc<RwLock<HostDispatcher>>, adapter: Arc<RwLock<HostDevice>>
-) -> impl Future<Item = AdapterEventStream, Error = fidl::Error> {
-    make_clones!(adapter => stream_adapter, adapter);
-    let stream = stream_adapter.read().adapter.take_event_stream();
-    stream
+pub fn run(
+    hd: Arc<RwLock<HostDispatcher>>, host: Arc<RwLock<HostDevice>>
+) -> impl Future<Item = HostEventStream, Error = fidl::Error> {
+    make_clones!(host => host_stream, host);
+    let stream = host_stream.read().host.take_event_stream();
+    stream 
         .for_each(move |evt| {
             match evt {
-                AdapterEvent::OnAdapterStateChanged { ref state } => {
-                    adapter.write().info.state = Some(Box::new(clone_adapter_state(&state)));
+                HostEvent::OnAdapterStateChanged { ref state } => {
+                    host.write().info.state = Some(Box::new(clone_host_state(&state)));
                 }
-                AdapterEvent::OnDeviceDiscovered { mut device } => {
+                HostEvent::OnDeviceDiscovered { mut device } => {
                     if let Some(ref events) = hd.write().events {
                         let _res = events
                             .send_on_device_updated(&mut device)

drivers/bluetooth/host/fidl/host_server.cc
@@ -4,20 +4,32 @@
 
 #include "host_server.h"
 
-#include "garnet/drivers/bluetooth/host/gatt_host.h"
+#include "garnet/drivers/bluetooth/lib/gap/adapter.h"
 
-#include "adapter_server.h"
+#include "garnet/drivers/bluetooth/host/gatt_host.h"
+#include "garnet/drivers/bluetooth/lib/gap/bredr_connection_manager.h"
+#include "garnet/drivers/bluetooth/lib/gap/bredr_discovery_manager.h"
+#include "garnet/drivers/bluetooth/lib/gap/gap.h"
+#include "garnet/drivers/bluetooth/lib/gap/low_energy_discovery_manager.h"
+#include "lib/fxl/logging.h"
+
 #include "helpers.h"
 #include "low_energy_central_server.h"
 #include "low_energy_peripheral_server.h"
 
 namespace bthost {
+using bluetooth::Bool;
+using bluetooth::ErrorCode;
+using bluetooth::Status;
+
+using bluetooth_control::AdapterState;
 
 HostServer::HostServer(zx::channel channel,
                        fxl::WeakPtr<::btlib::gap::Adapter> adapter,
                        fbl::RefPtr<GattHost> gatt_host)
     : AdapterServerBase(adapter, this, std::move(channel)),
-      gatt_host_(gatt_host) {
+      gatt_host_(gatt_host),
+      weak_ptr_factory_(this) {
   FXL_DCHECK(gatt_host_);
 }
 
@@ -25,9 +37,179 @@ void HostServer::GetInfo(GetInfoCallback callback) {
   callback(fidl_helpers::NewAdapterInfo(*adapter()));
 }
 
-void HostServer::RequestAdapter(
-    fidl::InterfaceRequest<bluetooth_host::Adapter> request) {
-  BindServer<AdapterServer>(std::move(request));
+void HostServer::SetLocalName(::fidl::StringPtr local_name,
+                              SetLocalNameCallback callback) {
+  adapter()->SetLocalName(local_name, [self = weak_ptr_factory_.GetWeakPtr(),
+                                       callback](auto status) {
+    callback(fidl_helpers::StatusToFidl(status, "Can't Set Local Name"));
+  });
+}
+
+void HostServer::StartDiscovery(StartDiscoveryCallback callback) {
+  FXL_VLOG(1) << "Adapter StartDiscovery()";
+  FXL_DCHECK(adapter());
+
+  if (le_discovery_session_ || requesting_discovery_) {
+    FXL_VLOG(1) << "Discovery already in progress";
+    callback(fidl_helpers::NewFidlError(ErrorCode::IN_PROGRESS,
+                                        "Discovery already in progress"));
+    return;
+  }
+
+  requesting_discovery_ = true;
+  auto bredr_manager = adapter()->bredr_discovery_manager();
+  // TODO(jamuraa): start these in parallel instead of sequence
+  bredr_manager->RequestDiscovery([self = weak_ptr_factory_.GetWeakPtr(),
+                                   bredr_manager, callback](
+                                      btlib::hci::Status status, auto session) {
+    if (!self) {
+      callback(
+          fidl_helpers::NewFidlError(ErrorCode::FAILED, "Adapter Shutdown"));
+      return;
+    }
+
+    if (!status || !session) {
+      FXL_VLOG(1) << "Failed to start BR/EDR discovery session";
+      callback(fidl_helpers::StatusToFidl(
+          status, "Failed to start BR/EDR discovery session"));
+      self->requesting_discovery_ = false;
+      return;
+    }
+
+    session->set_result_callback([self](const auto& device) {
+      if (self) {
+        self->OnDiscoveryResult(device);
+      }
+    });
+
+    self->bredr_discovery_session_ = std::move(session);
+
+    auto le_manager = self->adapter()->le_discovery_manager();
+    le_manager->StartDiscovery([self, callback](auto session) {
+      // End the new session if this AdapterServer got destroyed in the mean
+      // time (e.g. because the client disconnected).
+      if (!self) {
+        callback(
+            fidl_helpers::NewFidlError(ErrorCode::FAILED, "Adapter Shutdown"));
+        return;
+      }
+
+      if (!session) {
+        FXL_VLOG(1) << "Failed to start LE discovery session";
+        callback(fidl_helpers::NewFidlError(
+            ErrorCode::FAILED, "Failed to start LE discovery session"));
+        self->bredr_discovery_session_ = nullptr;
+        self->requesting_discovery_ = false;
+        return;
+      }
+
+      // Set up a general-discovery filter for connectable devices.
+      session->filter()->set_connectable(true);
+      session->filter()->SetGeneralDiscoveryFlags();
+      session->SetResultCallback([self](const auto& device) {
+        if (self) {
+          self->OnDiscoveryResult(device);
+        }
+      });
+
+      self->le_discovery_session_ = std::move(session);
+      self->requesting_discovery_ = false;
+
+      // Send the adapter state update.
+      AdapterState state;
+      state.discovering = bluetooth::Bool::New();
+      state.discovering->value = true;
+      self->binding()->events().OnAdapterStateChanged(std::move(state));
+
+      callback(Status());
+    });
+  });
+}
+
+void HostServer::StopDiscovery(StopDiscoveryCallback callback) {
+  FXL_VLOG(1) << "Adapter StopDiscovery()";
+  if (!le_discovery_session_) {
+    FXL_VLOG(1) << "No active discovery session";
+    callback(fidl_helpers::NewFidlError(ErrorCode::BAD_STATE,
+                                        "No discovery session in progress"));
+    return;
+  }
+
+  bredr_discovery_session_ = nullptr;
+  le_discovery_session_ = nullptr;
+
+  AdapterState state;
+  state.discovering = bluetooth::Bool::New();
+  state.discovering->value = false;
+  this->binding()->events().OnAdapterStateChanged(std::move(state));
+
+  callback(Status());
+}
+
+void HostServer::SetConnectable(bool connectable,
+                                SetConnectableCallback callback) {
+  FXL_VLOG(1) << "Adapter SetConnectable(" << connectable << ")";
+
+  adapter()->bredr_connection_manager()->SetConnectable(
+      connectable, [callback](const auto& status) {
+        callback(fidl_helpers::StatusToFidl(status));
+      });
+}
+
+void HostServer::SetDiscoverable(bool discoverable,
+                                 SetDiscoverableCallback callback) {
+  FXL_VLOG(1) << "Adapter SetDiscoverable(" << discoverable << ")";
+  // TODO(NET-830): advertise LE here
+  if (!discoverable) {
+    bredr_discoverable_session_ = nullptr;
+
+    AdapterState state;
+    state.discoverable = bluetooth::Bool::New();
+    state.discoverable->value = false;
+    this->binding()->events().OnAdapterStateChanged(std::move(state));
+
+    callback(Status());
+  }
+  if (discoverable && requesting_discoverable_) {
+    FXL_VLOG(1) << "Discoverable already being set";
+    callback(fidl_helpers::NewFidlError(ErrorCode::IN_PROGRESS,
+                                        "Discovery already in progress"));
+    return;
+  }
+  requesting_discoverable_ = true;
+  auto bredr_manager = adapter()->bredr_discovery_manager();
+  bredr_manager->RequestDiscoverable(
+      [self = weak_ptr_factory_.GetWeakPtr(), callback](
+          btlib::hci::Status status, auto session) {
+        if (!self) {
+          callback(fidl_helpers::NewFidlError(ErrorCode::FAILED,
+                                              "Adapter Shutdown"));
+          return;
+        }
+        if (!status || !session) {
+          FXL_VLOG(1) << "Failed to set discoverable!";
+          callback(
+              fidl_helpers::StatusToFidl(status, "Failed to set discoverable"));
+          self->requesting_discoverable_ = false;
+        }
+        self->bredr_discoverable_session_ = std::move(session);
+        AdapterState state;
+        state.discoverable = bluetooth::Bool::New();
+        state.discoverable->value = true;
+        self->binding()->events().OnAdapterStateChanged(std::move(state));
+        callback(Status());
+      });
+}
+
+void HostServer::OnDiscoveryResult(
+    const ::btlib::gap::RemoteDevice& remote_device) {
+  auto fidl_device = fidl_helpers::NewRemoteDevice(remote_device);
+  if (!fidl_device) {
+    FXL_VLOG(1) << "Ignoring malformed discovery result";
+    return;
+  }
+
+  this->binding()->events().OnDeviceDiscovered(std::move(*fidl_device));
 }
 
 void HostServer::RequestLowEnergyCentral(