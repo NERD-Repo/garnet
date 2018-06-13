3b62d3ad0106a2419d2229c2bd27da93aa7d7ce8
Rob Tsuk <robtsuk@google.com>
2018-06-13 15:33:49 -0700

    WIP
    Change-Id: Ibf73f787239b067e3a9f468da98e81e48cd2a668
    
    # Conflicts:
    #	.gitignore
    #	packages/examples/scenic

--- examples/ui/spinning_square_rs/src/main.rs
+++ examples/ui/spinning_square_rs/src/main.rs
@@ -2,6 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-fn main() {
-    println!("Hello, world!");
+extern crate fidl;
+extern crate failure;
+extern crate fuchsia_app as component;
+extern crate fuchsia_async as async;
+extern crate futures;
+
+use failure::{Error, ResultExt};
+use futures::future::empty;
+
+fn main() -> Result<(), Error> {
+    let mut executor = async::Executor::new().context("Error creating executor")?;
+
+    executor.run_singlethreaded(empty::<(), Error>())?;
+
+    Ok(())
 }
