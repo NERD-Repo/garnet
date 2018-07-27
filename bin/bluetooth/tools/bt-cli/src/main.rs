7c5f874728463157c528597933b0fe9fc92279b8
Benjamin Brittain <bwb@google.com>
2018-07-27 13:27:36 -0700

    bt-cli async/awit
    Change-Id: I8d769b1828eed5a77abc86aa01d4fcf53017f4d9

--- bin/bluetooth/tools/bt-cli/src/main.rs
+++ bin/bluetooth/tools/bt-cli/src/main.rs
@@ -2,103 +2,83 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+#![feature(rust_2018_preview)]
+#![feature(async_await, await_macro, futures_api)]
 #![deny(warnings)]
 
-extern crate failure;
-extern crate fidl_fuchsia_bluetooth_control;
 extern crate fuchsia_app as app;
-#[macro_use]
-extern crate fuchsia_async as async;
+extern crate fuchsia_async as fasync;
 extern crate fuchsia_bluetooth as bluetooth;
-extern crate fuchsia_zircon as zircon;
-extern crate futures;
-extern crate parking_lot;
-extern crate rustyline;
 
-use app::client::connect_to_service;
-use bluetooth::types::Status;
-use commands::{Cmd, CmdCompleter};
-use failure::{Fail, ResultExt};
+use crate::app::client::connect_to_service;
+use crate::bluetooth::types::Status;
+use crate::commands::{Cmd, CmdCompleter};
+use failure::ResultExt;
 use failure::Error;
-use fidl_fuchsia_bluetooth_control::{ControlEvent, ControlMarker, ControlProxy};
-use futures::{Future, FutureExt, StreamExt};
-use futures::future::ok as fok;
+use fidl_fuchsia_bluetooth_control::{ControlEvent, ControlMarker, ControlProxy, ControlEventStream};
+use crate::fasync::futures::{TryFutureExt, StreamExt};
 use parking_lot::RwLock;
 use rustyline::{CompletionType, Config, EditMode, Editor};
 use rustyline::error::ReadlineError;
 use std::fmt::Write;
 use std::sync::Arc;
-use types::AdapterInfo;
+use crate::types::AdapterInfo;
 
 mod commands;
 mod types;
 
 static PROMPT: &'static str = "\x1b[34mbt>\x1b[0m ";
 
-fn get_active_adapter(
-    control_svc: Arc<RwLock<ControlProxy>>
-) -> impl Future<Item = String, Error = Error> {
-    control_svc
-        .read()
-        .get_active_adapter_info()
-        .map_err(|e| e.context("error getting response").into())
-        .and_then(|response| {
-            Ok(match response {
-                None => String::from("No Active Adapter"),
-                Some(adapter) => AdapterInfo::from(*adapter).to_string(),
-            })
-        })
+async fn get_active_adapter(control_svc: Arc<RwLock<ControlProxy>>) -> Result<String, Error> {
+    if let Some(adapter) = await!(control_svc.read().get_active_adapter_info())? {
+        return Ok(AdapterInfo::from(*adapter).to_string())
+    }
+    Ok(String::from("No Active Adapter"))
 }
 
-fn get_adapters(
-    control_svc: Arc<RwLock<ControlProxy>>
-) -> impl Future<Item = String, Error = Error> {
-    control_svc
-        .read()
-        .get_adapters()
-        .map_err(|e| e.context("error getting response").into())
-        .and_then(|response| {
-            let mut string = String::new();
-            match response {
-                Some(adapters) => {
-                    for adapter in adapters {
-                        let _ = writeln!(string, "{}", AdapterInfo::from(adapter));
-                    }
-                    Ok(string)
-                }
-                None => Ok(String::from("No adapters detected")),
+async fn get_adapters(control_svc: Arc<RwLock<ControlProxy>>) -> Result<String, Error> {
+    if let Some(adapters) = await!(control_svc.read().get_adapters())? {
+        let mut string = String::new();
+        for adapter in adapters {
+            let _ = writeln!(string, "{}", AdapterInfo::from(adapter));
+        }
+        return Ok(string)
+    }
+    Ok(String::from("No adapters detected"))
+}
+
+async fn start_discovery(control_svc: Arc<RwLock<ControlProxy>>) -> Result<String, Error> {
+    let response = await!(control_svc.read().request_discovery(true))?;
+    Ok(Status::from(response).to_string())
+}
+
+async fn stop_discovery(control_svc: Arc<RwLock<ControlProxy>>) -> Result<String, Error> {
+    let response = await!(control_svc.read().request_discovery(false))?;
+    Ok(Status::from(response).to_string())
+}
+
+async fn set_discoverable(discoverable: bool, control_svc: Arc<RwLock<ControlProxy>>) -> Result<String, Error> {
+    let response = await!(control_svc.read().set_discoverable(discoverable))?;
+    Ok(Status::from(response).to_string())
+}
+
+async fn run_listeners(mut stream: ControlEventStream) -> Result<(), Error> {
+    while let Some(evt) = await!(stream.next()) {
+        match evt? {
+            ControlEvent::OnAdapterUpdated { adapter } => {
+                eprintln!("Adapter {} updated", adapter.identifier);
             }
-        })
-}
-
-fn start_discovery(
-    control_svc: Arc<RwLock<ControlProxy>>
-) -> impl Future<Item = String, Error = Error> {
-    control_svc
-        .read()
-        .request_discovery(true)
-        .map_err(|e| e.context("error getting response").into())
-        .and_then(|response| Ok(Status::from(response).to_string()))
-}
-
-fn stop_discovery(
-    control_svc: Arc<RwLock<ControlProxy>>
-) -> impl Future<Item = String, Error = Error> {
-    control_svc
-        .read()
-        .request_discovery(false)
-        .map_err(|e| e.context("error getting response").into())
-        .and_then(|response| Ok(Status::from(response).to_string()))
-}
-
-fn set_discoverable(discoverable: bool,
-    control_svc: Arc<RwLock<ControlProxy>>
-) -> impl Future<Item = String, Error = Error> {
-    control_svc
-        .read()
-        .set_discoverable(discoverable)
-        .map_err(|e| e.context("error getting response").into())
-        .and_then(|response| Ok(Status::from(response).to_string()))
+            ControlEvent::OnDeviceUpdated { device } => {
+                eprintln!("Device: {:#?}", device);
+            }
+            ControlEvent::OnDeviceRemoved { identifier } => {
+                eprintln!("Device {} removed", identifier);
+            }
+            _ => eprintln!("Unknown Event"),
+        }
+    }
+    Ok(())
 }
 
 fn main() -> Result<(), Error> {
@@ -111,34 +91,14 @@ fn main() -> Result<(), Error> {
     let mut rl = Editor::with_config(config);
     rl.set_completer(Some(c));
 
-    let mut exec = async::Executor::new().context("error creating event loop")?;
+    let mut exec = fasync::Executor::new().context("error creating event loop")?;
     let bt_svc = Arc::new(RwLock::new(connect_to_service::<ControlMarker>()
         .context("failed to connect to bluetooth control interface")?));
 
     let bt_svc_thread = bt_svc.clone();
     let evt_stream = bt_svc_thread.read().take_event_stream();
 
-    // Start listening for events
-    async::spawn(
-        evt_stream
-            .for_each(move |evt| {
-                match evt {
-                    ControlEvent::OnAdapterUpdated { adapter } => {
-                        eprintln!("Adapter {} updated", adapter.identifier);
-                    }
-                    ControlEvent::OnDeviceUpdated { device } => {
-                        eprintln!("Device: {:#?}", device);
-                    }
-                    ControlEvent::OnDeviceRemoved { identifier } => {
-                        eprintln!("Device {} removed", identifier);
-                    }
-                    _ => eprintln!("Unknown Event: {:#?}", evt),
-                }
-                fok(())
-            })
-            .map(|_| ())
-            .recover(|e| eprintln!("failed to subscribe to bluetooth events: {:?}", e)),
-    );
+    fasync::spawn(run_listeners(evt_stream).unwrap_or_else(|e| eprintln!("Failed to subscribe to bluetooth events {:?}", e)));
 
     // start the repl
     loop {
@@ -146,42 +106,30 @@ fn main() -> Result<(), Error> {
         match readline {
             Ok(line) => {
                 let cmd = line.parse::<Cmd>();
-                many_futures!(
-                    Output,
-                    [
-                        StartDiscovery,
-                        StopDiscovery,
-                        Discoverable,
-                        GetAdapters,
-                        GetActiveAdapter,
-                        Help,
-                        Error
-                    ]
-                );
-                let fut = match cmd {
-                    Ok(Cmd::StartDiscovery) => {
-                        println!("Starting Discovery!");
-                        Output::StartDiscovery(start_discovery(bt_svc.clone()))
+                let fut = async {
+                    match cmd {
+                        Ok(Cmd::StartDiscovery) => {
+                            println!("Starting Discovery!");
+                            await!(start_discovery(bt_svc.clone()))
+                        }
+                        Ok(Cmd::StopDiscovery) => {
+                            println!("Stopping Discovery!");
+                            await!(stop_discovery(bt_svc.clone()))
+                        }
+                        Ok(Cmd::Discoverable) => {
+                            println!("Becoming discoverable..");
+                            await!(set_discoverable(true, bt_svc.clone()))
+                        }
+                        Ok(Cmd::NotDiscoverable) => {
+                            println!("Revoking discoverability..");
+                            await!(set_discoverable(false, bt_svc.clone()))
+                        }
+                        Ok(Cmd::GetAdapters) => await!(get_adapters(bt_svc.clone())),
+                        Ok(Cmd::ActiveAdapter) =>  await!(get_active_adapter(bt_svc.clone())),
+                        Ok(Cmd::Help) => await!({ async { Ok(Cmd::help_msg()) } }),
+                        Ok(Cmd::Nothing) => await!({async { Ok(String::from("")) }}),
+                        Err(e) => await!({ async { Ok(format!("Error: {:?}", e)) } })
                     }
-                    Ok(Cmd::StopDiscovery) => {
-                        println!("Stopping Discovery!");
-                        Output::StopDiscovery(stop_discovery(bt_svc.clone()))
-                    }
-                    Ok(Cmd::Discoverable) => {
-                        println!("Becoming discoverable..");
-                        Output::Discoverable(set_discoverable(true, bt_svc.clone()))
-                    }
-                    Ok(Cmd::NotDiscoverable) => {
-                        println!("Revoking discoverability..");
-                        Output::Discoverable(set_discoverable(false, bt_svc.clone()))
-                    }
-                    Ok(Cmd::GetAdapters) => Output::GetAdapters(get_adapters(bt_svc.clone())),
-                    Ok(Cmd::ActiveAdapter) => {
-                        Output::GetActiveAdapter(get_active_adapter(bt_svc.clone()))
-                    }
-                    Ok(Cmd::Help) => Output::Help(fok(Cmd::help_msg())),
-                    Ok(Cmd::Nothing) => Output::Error(fok(String::from(""))),
-                    Err(e) => Output::Error(fok(format!("Error: {:?}", e))),
                 };
                 let res = exec.run_singlethreaded(fut)?;
                 if res != "" {
