69247e52cbc4b0f90ddd53ea1f8c8d64732c50af
Benjamin Brittain <bwb@google.com>
2018-07-28 13:37:30 -0700

    convert sl4f to await
    Change-Id: I7a9e8146bb390462cf4b5aa10742fa94d5797095

--- bin/sl4f/src/common/sl4f_executor.rs
+++ bin/sl4f/src/common/sl4f_executor.rs
@@ -2,55 +2,55 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use async;
+use crate::fasync;
 use failure::ResultExt;
 use futures::channel::mpsc;
-use futures::{FutureExt, StreamExt};
+use futures::StreamExt;
 use parking_lot::RwLock;
 use std::sync::Arc;
 
-use common::sl4f::method_to_fidl;
-use common::sl4f::Sl4f;
-use common::sl4f_types::{AsyncRequest, AsyncResponse};
+use crate::common::sl4f::method_to_fidl;
+use crate::common::sl4f::Sl4f;
+use crate::common::sl4f_types::{AsyncRequest, AsyncResponse};
 
 pub fn run_fidl_loop(
-    sl4f_session: Arc<RwLock<Sl4f>>, receiver: mpsc::UnboundedReceiver<AsyncRequest>,
+    sl4f_session: Arc<RwLock<Sl4f>>, mut receiver: mpsc::UnboundedReceiver<AsyncRequest>,
 ) {
-    let mut executor = async::Executor::new()
+    let mut executor = fasync::Executor::new()
         .context("Error creating event loop")
         .expect("Failed to create an executor!");
 
-    let receiver_fut = receiver.for_each_concurrent(move |request| match request {
-        AsyncRequest {
-            tx,
-            id,
-            method_type,
-            name,
-            params,
-        } => {
-            let curr_sl4f_session = sl4f_session.clone();
-            fx_log_info!(tag: "run_fidl_loop",
-                "Received synchronous request: {:?}, {:?}, {:?}, {:?}, {:?}",
-                tx, id, method_type, name, params
-            );
+    // TODO another pattern for concurrent?
 
-            let fidl_fut = method_to_fidl(
-                method_type.clone(),
-                name.clone(),
-                params.clone(),
-                curr_sl4f_session.clone(),
-            );
-            fidl_fut.and_then(move |resp| {
-                let response = AsyncResponse::new(resp);
+    let fut = async {
+        while let Some(request) = await!(receiver.next()) {
+            match request {
+                AsyncRequest {
+                    tx,
+                    id,
+                    method_type,
+                    name,
+                    params,
+                } => {
+                    let curr_sl4f_session = sl4f_session.clone();
+                    fx_log_info!(tag: "run_fidl_loop",
+                                 "Received synchronous request: {:?}, {:?}, {:?}, {:?}, {:?}",
+                                 tx, id, method_type, name, params
+                                );
 
-                // Ignore any tx sending errors, other requests can still be outstanding
-                let _ = tx.send(response);
-                Ok(())
-            })
+                    let resp = await!(method_to_fidl(
+                        method_type.clone(),
+                        name.clone(),
+                        params.clone(),
+                        curr_sl4f_session.clone()));
+                    let response = AsyncResponse::new(resp);
+
+                    // Ignore any tx sending errors, other requests can still be outstanding
+                    let _ = tx.send(response);
+                }
+            }
         }
-    });
+    };
 
-    executor
-        .run_singlethreaded(receiver_fut)
-        .expect("Failed to execute requests from Rouille.");
+    executor.run_singlethreaded(fut);
 }
